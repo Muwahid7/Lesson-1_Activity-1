08643eed1e5ea99711f0f00c3b3e6ecef971ce58
Muwahid7 <muwahid.md@gmail.com>
2025-05-15 12:48:31 +0530

    Add files via upload

---
+++ Storing birthdays.py
@@ -0,0 +1,14 @@
+person1_name = "Aman"
+person1_birthday = "5-12-2012"
+
+person2_name = "Harsh"
+person2_birthday = "11-09-2021"
+
+person3_name = "Patel"
+person3_birthday = "02-03-2011"
+
+person4_name = "Arush"
+person4_birthday = "05-04-2016"
+
+person5_name = "Stark"
+person5_birthday = "01-01-2001"
