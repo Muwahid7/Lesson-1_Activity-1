5fbbe0dc1080c1a99b10c8bcf9e002caf2b20382
Muwahid7 <muwahid.md@gmail.com>
2025-05-16 12:44:18 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,9 @@
+Subject_1 = 92
+Subject_2 = 95
+Subject_3 = 96
+Subject_4 = 94
+Subject_5 = 100
+Sum = Subject_1+Subject_2+Subject_3+Subject_4 +Subject_5
+print(Sum)
+average = Sum/5
+print(average)
---
+++ Percentage Calculator.py
@@ -0,0 +1,10 @@
+print("Enter marks obtained in the following Subjects")
+Math = int(input("Math"))
+Science = int(input("Science"))
+Social = int(input("Social"))
+Subject_4 = int(input("Subject_4 "))
+Subject_5 = int(input("Subject_5 "))
+Sum = Math+Science+Social+Subject_4+Subject_5
+print(Sum)
+Percentage = (Sum/500)*100
+print(Percentage)
