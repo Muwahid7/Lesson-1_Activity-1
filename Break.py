5d367267ac2373ca20d125dcb386e53ba3e6d152
Muwahid7 <muwahid.md@gmail.com>
2025-06-27 20:21:15 +0530

    Add files via upload

---
+++ Break.py
@@ -0,0 +1,8 @@
+Wend = input("Enter a word")
+for i in Wend:
+    if (Wend=='A'):
+        print("A is found")
+        break
+    else:
+        print("A is not found")
+        
---
+++ Continue.py
@@ -0,0 +1,7 @@
+Var=10
+while Var>0:
+    Var=Var-1
+    if Var==5:
+        continue
+    print("current variable Value",Var)
+print("Goodbye")
