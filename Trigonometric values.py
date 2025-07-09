4001d9ba5b6038f3f93ea78c9685fe8bb49c7867
Muwahid7 <muwahid.md@gmail.com>
2025-07-09 20:51:38 +0530

    Add files via upload

---
+++ Trigonometric values.py
@@ -0,0 +1,6 @@
+import math
+angle_deg = float(input("Enter angle in degrees: "))
+angle_rad = math.radians(angle_deg)
+print("sine value",math.sin(angle_rad))
+print("cosine value",math.cos(angle_rad))
+print("tangent value",math.tan(angle_rad))
